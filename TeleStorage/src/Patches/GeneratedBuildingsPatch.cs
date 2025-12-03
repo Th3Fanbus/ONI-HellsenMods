9c8ba4247b3898f034426c9788714e88a84eafd1
Angel Pons <th3fanbus@gmail.com>
2025-12-03 20:34:16 +0100

    TeleStorage: Remove unused leftover code
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>

--- TeleStorage/src/Patches/GeneratedBuildingsPatch.cs
+++ TeleStorage/src/Patches/GeneratedBuildingsPatch.cs
@@ -8,15 +8,7 @@ namespace TeleStorage
 		{
 			if (TUNING.BUILDINGS.PLANSUBCATEGORYSORTING.ContainsKey(relativeBuildingId)) {
 				string subcategoryID = TUNING.BUILDINGS.PLANSUBCATEGORYSORTING[relativeBuildingId];
-#if true
 				TUNING.BUILDINGS.PLANSUBCATEGORYSORTING[building_id] = subcategoryID;
-#else
-				if (TUNING.BUILDINGS.PLANSUBCATEGORYSORTING.ContainsKey(building_id)) {
-					TUNING.BUILDINGS.PLANSUBCATEGORYSORTING[building_id] = subcategoryID;
-				} else {
-					TUNING.BUILDINGS.PLANSUBCATEGORYSORTING.Add(building_id, subcategoryID);
-				}
-#endif
 				ModUtil.AddBuildingToPlanScreen(category, building_id, subcategoryID, relativeBuildingId);
 			} else {
 				ModUtil.AddBuildingToPlanScreen(category, building_id);
@@ -28,7 +20,6 @@ namespace TeleStorage
 		{
 			public static void Postfix()
 			{
-
 				AddBuildingToPlanScreenBehindNext("Base", TeleStorageLiquidConfig.Id, LiquidReservoirConfig.ID);
 				AddBuildingToPlanScreenBehindNext("Base", TeleStorageGasConfig.Id, GasReservoirConfig.ID);
 			}
