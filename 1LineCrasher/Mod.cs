1c403818321d42a752224beacea1688ff955c4d2
Angel Pons <th3fanbus@gmail.com>
2026-04-03 18:32:43 +0200

    1LineCrasher: Remove dead code
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>

--- 1LineCrasher/Mod.cs
+++ 1LineCrasher/Mod.cs
@@ -1,13 +1,3 @@
-﻿
-
-namespace _1LineCrasher
+﻿namespace _1LineCrasher
 {
-	//public class Mod : UserMod2
-	//{
-	//    public override void OnLoad(Harmony harmony)
-	//    {
-	//        base.OnLoad(harmony);
-	//    }
-	//}
-
 }
